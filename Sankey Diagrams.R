deab32532dc997e1964dd60cbf987cc5ed4d6ca8
Jackson Hammond <jbhammon@users.noreply.github.com>
2018-12-16 20:44:54 -0500

    Sankey diagram data structure updates
    Reworked how the data structures for the sankey diagrams are being built. Was able to succesfully make a Sankey diagram for all the "Bicep" exercises.

--- Sankey Diagrams.R
+++ Sankey Diagrams.R
@@ -1,28 +1,21 @@
-fitnessData<- read.csv("~/Fitness/Portfolio Project/Workout-Visualizations/FitNotes_Export_2018_10_12_15_16_59.csv")
+fitnessData<- read.csv("~/Fitness/Portfolio Project/Workout-Visualizations/FitNotes_Export_Latest.csv")
 library(networkD3)
 
-fitnessData
 fitnessData$Date = as.Date(fitnessData$Date, format = "%m/%d/%Y")
 fitnessData
 
-fitnessData2018 = fitnessData[fitnessData$Date > "2017-12-31",]
+data2018 = fitnessData[fitnessData$Date > "2017-12-31",]
 
-totalReps = data.frame(aggregate(fitnessData2018$Reps, by = list(Exercise = fitnessData2018$Exercise), FUN = sum))
+nextCategory = data2018[data2018$Category == "Biceps",]
+totalVolume = data.frame(aggregate(nextCategory$Weight..lbs., by = list(Exercise = nextCategory$Exercise), FUN = sum))
 
-totalReps
+totalVolume$fromID = 0:(nrow(totalVolume)-1)
+totalVolume$toID = max(totalVolume$fromID) + 1
 
-bodyCategories = data.frame(unique(fitnessData2018$Category))
+nodes = rbind(data.frame("name" = totalVolume$Exercise), data.frame("name" = "Biceps"))
 
-totalReps$ID = 0:(nrow(totalReps)-1)
+links = as.data.frame(matrix(c(totalVolume$fromID, totalVolume$toID, totalVolume$x), ncol = 3))
 
-nodes = data.frame("name" = totalReps$Category)
-
-links = as.data.frame(matrix(c(
-  0, 1, 10, # Each row represents a link. The first number
-  0, 2, 20, # represents the node being conntected from. 
-  1, 3, 30, # the second number represents the node connected to.
-  2, 3, 40),# The third number is the value of the node
-  byrow = TRUE, ncol = 3))
 names(links) = c("source", "target", "value")
 sankeyNetwork(Links = links, Nodes = nodes,
               Source = "source", Target = "target",
@@ -31,21 +24,23 @@ sankeyNetwork(Links = links, Nodes = nodes,
 
 ###############################################################
 ## This Stuff Works
+##
+## Saving as a reference for using sankeyNetwork()
 ###############################################################
-nodes = data.frame("name" =
-                     c("Node A", # Node 0
-                       "Node B", # Node 1
-                       "Node C", # Node 2
-                       "Node D"))# Node 3
-links = as.data.frame(matrix(c(
-  0, 1, 10, # Each row represents a link. The first number
-  0, 2, 20, # represents the node being conntected from.
-  1, 3, 30, # the second number represents the node connected to.
-  2, 3, 40),# The third number is the value of the node
-  byrow = TRUE, ncol = 3))
-names(links) = c("source", "target", "value")
-sankeyNetwork(Links = links, Nodes = nodes,
-              Source = "source", Target = "target",
-              Value = "value", NodeID = "name",
-              fontSize= 12, nodeWidth = 30)
+# nodes = data.frame("name" =
+#                      c("Node A", # Node 0
+#                        "Node B", # Node 1
+#                        "Node C", # Node 2
+#                        "Node D"))# Node 3
+# links = as.data.frame(matrix(c(
+#   0, 1, 10, # Each row represents a link. The first number
+#   0, 2, 20, # represents the node being conntected from.
+#   1, 3, 30, # the second number represents the node connected to.
+#   2, 3, 40),# The third number is the value of the node
+#   byrow = TRUE, ncol = 3))
+# names(links) = c("source", "target", "value")
+# sankeyNetwork(Links = links, Nodes = nodes,
+#               Source = "source", Target = "target",
+#               Value = "value", NodeID = "name",
+#               fontSize= 12, nodeWidth = 30)
 
